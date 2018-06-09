829a24ba4051cd588051bccbc79bae02ce79fec4
Fetter-poi <oox_x44@163.com>
2018-06-10 02:36:55 +0800

    Update uplod.it.php

--- hosts/upload/uplod.it.php
+++ hosts/upload/uplod.it.php
@@ -19,8 +19,8 @@ $_T8['anonUploadLimit'] = 0; // File-size limit for non-registered users (MB) | 
 $acc_key_name = str_ireplace(array('www.', '.'), array('', '_'), $_T8['domain']); // (Do Not Edit)
 
 /* # Account Info # */
-$upload_acc[$acc_key_name]['user'] = ''; //Set your login
-$upload_acc[$acc_key_name]['pass'] = ''; //Set your password
+$upload_acc[$acc_key_name]['user'] = 'uploads@emmm.anyalias.com'; //Set your login
+$upload_acc[$acc_key_name]['pass'] = 'uploads'; //Set your password
 
 if (!file_exists(HOST_DIR . 'upload/GenericXFSHost.inc.php')) html_error('Cannot load "'.htmlentities(HOST_DIR).'upload/GenericXFSHost.inc.php" (File doesn\'t exists), please install lastest version from: http://rapidleech.com/forum/viewtopic.php?f=17&t=80 or http://pastebin.com/E0z7qMU1 ');
 require(HOST_DIR . 'upload/GenericXFSHost.inc.php');
